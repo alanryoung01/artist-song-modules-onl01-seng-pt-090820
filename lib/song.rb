bce853fce7a4fca421d0abc7c4ece5f067d84341
Alan Young <alanryoung01@gmail.com>
2020-10-07 09:07:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,11 +4,6 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  extend Memorable::ClassMethods
-  extend Findable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
-
   @@songs = []
 
   def initialize
